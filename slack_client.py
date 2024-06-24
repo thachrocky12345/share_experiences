3d8554b6a566aa069c35f5f54334d4b6a436da62
thachrocky12345 <thachrocky@icloud.com>
2024-06-24 16:35:51 -0500

    Create slack_client.py

---
+++ slack_client.py
@@ -0,0 +1,76 @@
+import os
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+# Set your Slack API token here
+# SLACK_API_TOKEN = 'your-slack-api-token'  # Replace with your actual token
+SLACK_API_TOKEN = 'TOKEN'  # Replace with your actual token
+
+# Initialize the Slack client
+client = WebClient(token=SLACK_API_TOKEN)
+
+# Function to get the last message from a Slack channel
+def get_last_message(channel_id):
+    try:
+        response = client.conversations_history(
+            channel=channel_id,
+            limit=5  # Get only the last message
+        )
+        print(response)
+        messages = response['messages']
+        if messages:
+            last_message = messages[0]
+            print(f"Last message in {channel_id}: {last_message['text']}")
+        else:
+            print(f"No messages found in {channel_id}")
+    except SlackApiError as e:
+        print(f"Error fetching messages: {e.response['error']}")
+        
+# Function to send a message to a Slack channel
+def send_message(channel, text):
+    try:
+        response = client.chat_postMessage(
+            channel=channel,
+            text=text
+        )
+        print(f"Message sent to {channel}: {response['message']['text']}")
+    except SlackApiError as e:
+        print(f"Error sending message: {e.response['error']}")
+
+def send_block_message(channel_id, blocks):
+    try:
+        response = client.chat_postMessage(
+            channel=channel_id,
+            blocks=blocks
+        )
+        print(f"Message sent to {channel_id}: {response['ts']}")
+    except SlackApiError as e:
+        print(f"Error sending message: {e.response['error']}")
+        
+# Usage example
+if __name__ == "__main__":
+    channel = '#general'  # Replace with your channel ID or name
+    text = "test"
+    blocks = [
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "Test block with user https://gmatches.com select"
+			},
+			"accessory": {
+				"type": "users_select",
+				"placeholder": {
+					"type": "plain_text",
+					"text": "Select a user",
+					"emoji": True
+				},
+				"action_id": "users_select-action"
+			}
+		}
+	]
+    send_block_message(channel, blocks)
+    
+    # for channel_id in ('C079AGDBEUT','C079D2ZUNSY'):  # Replace with your channel ID
+    
+    #     get_last_message(channel_id)
